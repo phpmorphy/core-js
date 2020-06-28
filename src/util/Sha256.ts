37a8f5e1f554a1a7a390bf0250796e719b290488
UMI <dev@umi.top>
2020-06-28 17:01:47 +1200

    minor upd (3)

--- src/util/Sha256.ts
+++ src/util/Sha256.ts
@@ -35,21 +35,16 @@ function sha256 (message: Uint8Array): Uint8Array {
     0x510E527F, 0x9B05688C, 0x1F83D9AB, 0x5BE0CD19])
 
   const k = new Int32Array([
-    0x428A2F98, 0x71374491, 0xB5C0FBCF, 0xE9B5DBA5,
-    0x3956C25B, 0x59F111F1, 0x923F82A4, 0xAB1C5ED5,
-    0xD807AA98, 0x12835B01, 0x243185BE, 0x550C7DC3,
-    0x72BE5D74, 0x80DEB1FE, 0x9BDC06A7, 0xC19BF174,
-    0xE49B69C1, 0xEFBE4786, 0x0FC19DC6, 0x240CA1CC,
-    0x2DE92C6F, 0x4A7484AA, 0x5CB0A9DC, 0x76F988DA,
-    0x983E5152, 0xA831C66D, 0xB00327C8, 0xBF597FC7,
-    0xC6E00BF3, 0xD5A79147, 0x06CA6351, 0x14292967,
-    0x27B70A85, 0x2E1B2138, 0x4D2C6DFC, 0x53380D13,
-    0x650A7354, 0x766A0ABB, 0x81C2C92E, 0x92722C85,
-    0xA2BFE8A1, 0xA81A664B, 0xC24B8B70, 0xC76C51A3,
-    0xD192E819, 0xD6990624, 0xF40E3585, 0x106AA070,
-    0x19A4C116, 0x1E376C08, 0x2748774C, 0x34B0BCB5,
-    0x391C0CB3, 0x4ED8AA4A, 0x5B9CCA4F, 0x682E6FF3,
-    0x748F82EE, 0x78A5636F, 0x84C87814, 0x8CC70208,
+    0x428A2F98, 0x71374491, 0xB5C0FBCF, 0xE9B5DBA5, 0x3956C25B, 0x59F111F1,
+    0x923F82A4, 0xAB1C5ED5, 0xD807AA98, 0x12835B01, 0x243185BE, 0x550C7DC3,
+    0x72BE5D74, 0x80DEB1FE, 0x9BDC06A7, 0xC19BF174, 0xE49B69C1, 0xEFBE4786,
+    0x0FC19DC6, 0x240CA1CC, 0x2DE92C6F, 0x4A7484AA, 0x5CB0A9DC, 0x76F988DA,
+    0x983E5152, 0xA831C66D, 0xB00327C8, 0xBF597FC7, 0xC6E00BF3, 0xD5A79147,
+    0x06CA6351, 0x14292967, 0x27B70A85, 0x2E1B2138, 0x4D2C6DFC, 0x53380D13,
+    0x650A7354, 0x766A0ABB, 0x81C2C92E, 0x92722C85, 0xA2BFE8A1, 0xA81A664B,
+    0xC24B8B70, 0xC76C51A3, 0xD192E819, 0xD6990624, 0xF40E3585, 0x106AA070,
+    0x19A4C116, 0x1E376C08, 0x2748774C, 0x34B0BCB5, 0x391C0CB3, 0x4ED8AA4A,
+    0x5B9CCA4F, 0x682E6FF3, 0x748F82EE, 0x78A5636F, 0x84C87814, 0x8CC70208,
     0x90BEFFFA, 0xA4506CEB, 0xBEF9A3F7, 0xC67178F2
   ])
 
@@ -74,10 +69,8 @@ function sha256 (message: Uint8Array): Uint8Array {
     }
 
     for (let i = 16; i < 64; i++) {
-      const s0 = ((w[i - 15] >>> 7) | (w[i - 15] << 25)) ^
-        ((w[i - 15] >>> 18) | (w[i - 15] << 14)) ^ (w[i - 15] >>> 3)
-      const s1 = ((w[i - 2] >>> 17) | (w[i - 2] << 15)) ^
-        ((w[i - 2] >>> 19) | (w[i - 2] << 13)) ^ (w[i - 2] >>> 10)
+      const s0 = ((w[i - 15] >>> 7) | (w[i - 15] << 25)) ^ ((w[i - 15] >>> 18) | (w[i - 15] << 14)) ^ (w[i - 15] >>> 3)
+      const s1 = ((w[i - 2] >>> 17) | (w[i - 2] << 15)) ^ ((w[i - 2] >>> 19) | (w[i - 2] << 13)) ^ (w[i - 2] >>> 10)
       w[i] = w[i - 16] + s0 + w[i - 7] + s1
     }
 
@@ -91,12 +84,10 @@ function sha256 (message: Uint8Array): Uint8Array {
     let h = hh[7]
 
     for (let i = 0; i < 64; i++) {
-      const S0 = ((a >>> 2) | (a << 30)) ^ ((a >>> 13) | (a << 19)) ^
-        ((a >>> 22) | (a << 10))
+      const S0 = ((a >>> 2) | (a << 30)) ^ ((a >>> 13) | (a << 19)) ^ ((a >>> 22) | (a << 10))
       const Ma = (a & b) ^ (a & c) ^ (b & c)
       const t2 = S0 + Ma
-      const S1 = ((e >>> 6) | (e << 26)) ^ ((e >>> 11) | (e << 21)) ^
-        ((e >>> 25) | (e << 7))
+      const S1 = ((e >>> 6) | (e << 26)) ^ ((e >>> 11) | (e << 21)) ^ ((e >>> 25) | (e << 7))
       const Ch = (e & f) ^ ((~e) & g)
       const t1 = h + S1 + Ch + k[i] + w[i]
 
@@ -121,10 +112,14 @@ function sha256 (message: Uint8Array): Uint8Array {
   }
 
   const hash = new DataView(hh.buffer)
-
-  for (let i = 0; i < 8; i++) {
-    hash.setUint32(i * 4, hh[i])
-  }
+  hash.setUint32(0, hh[0])
+  hash.setUint32(4, hh[1])
+  hash.setUint32(8, hh[2])
+  hash.setUint32(12, hh[3])
+  hash.setUint32(16, hh[4])
+  hash.setUint32(20, hh[5])
+  hash.setUint32(24, hh[6])
+  hash.setUint32(28, hh[7])
 
   return new Uint8Array(hash.buffer)
 }
--- src/util/Utf8.ts
+++ src/util/Utf8.ts
@@ -34,18 +34,14 @@ function Utf8Encode (text: string): Uint8Array {
     if (code < 0x80) { // ascii
       bytes.push(code)
     } else if (code < 0x800) {
-      bytes.push(0xc0 | (code >> 6))
-      bytes.push(0x80 | (code & 0x3f))
+      bytes.push(0xc0 | (code >> 6), 0x80 | (code & 0x3f))
     } else if (code < 0xd800 || code >= 0xe000) {
-      bytes.push(0xe0 | (code >> 12))
-      bytes.push(0x80 | ((code >> 6) & 0x3f))
+      bytes.push((0xe0 | (code >> 12)), (0x80 | ((code >> 6) & 0x3f)))
       bytes.push(0x80 | (code & 0x3f))
     } else { // surrogate pair
       code = 0x10000 + ((code & 0x3ff) << 10) + (text.charCodeAt(++i) & 0x3ff)
-      bytes.push(0xf0 | (code >> 18))
-      bytes.push(0x80 | ((code >> 12) & 0x3f))
-      bytes.push(0x80 | ((code >> 6) & 0x3f))
-      bytes.push(0x80 | (code & 0x3f))
+      bytes.push((0xf0 | (code >> 18)), (0x80 | ((code >> 12) & 0x3f)))
+      bytes.push((0x80 | ((code >> 6) & 0x3f)), (0x80 | (code & 0x3f)))
     }
   }
 
@@ -72,8 +68,7 @@ function Utf8Decode (bytes: Uint8Array): string {
     } else { // surrogate pair
       const code = ((bytes[i] & 0x07) << 18 | (bytes[++i] & 0x3F) << 12 |
         (bytes[++i] & 0x3F) << 6 | bytes[++i] & 0x3F) - 0x010000
-      str += String.fromCharCode(code >> 10 | 0xD800)
-      str += String.fromCharCode(code & 0x03FF | 0xDC00)
+      str += String.fromCharCode(code >> 10 | 0xD800, code & 0x03FF | 0xDC00)
     }
   }
 
