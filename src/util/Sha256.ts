b6c9902ac54b4788dc0086dc5dae4a70ed68a337
UMI <dev@umi.top>
2020-06-30 13:53:34 +1200

    upd Sha256 (4)

--- src/util/Sha256.ts
+++ src/util/Sha256.ts
@@ -31,8 +31,7 @@
  */
 function sha256 (message: Uint8Array): Uint8Array {
   const hh = new Uint32Array([
-    0x6A09E667, 0xBB67AE85, 0x3C6EF372, 0xA54FF53A, 0x510E527F, 0x9B05688C, 0x1F83D9AB, 0x5BE0CD19
-  ])
+    0x6A09E667, 0xBB67AE85, 0x3C6EF372, 0xA54FF53A, 0x510E527F, 0x9B05688C, 0x1F83D9AB, 0x5BE0CD19])
 
   const k = new Uint32Array([
     0x428A2F98, 0x71374491, 0xB5C0FBCF, 0xE9B5DBA5, 0x3956C25B, 0x59F111F1, 0x923F82A4, 0xAB1C5ED5,
@@ -42,17 +41,11 @@ function sha256 (message: Uint8Array): Uint8Array {
     0x27B70A85, 0x2E1B2138, 0x4D2C6DFC, 0x53380D13, 0x650A7354, 0x766A0ABB, 0x81C2C92E, 0x92722C85,
     0xA2BFE8A1, 0xA81A664B, 0xC24B8B70, 0xC76C51A3, 0xD192E819, 0xD6990624, 0xF40E3585, 0x106AA070,
     0x19A4C116, 0x1E376C08, 0x2748774C, 0x34B0BCB5, 0x391C0CB3, 0x4ED8AA4A, 0x5B9CCA4F, 0x682E6FF3,
-    0x748F82EE, 0x78A5636F, 0x84C87814, 0x8CC70208, 0x90BEFFFA, 0xA4506CEB, 0xBEF9A3F7, 0xC67178F2
-  ])
+    0x748F82EE, 0x78A5636F, 0x84C87814, 0x8CC70208, 0x90BEFFFA, 0xA4506CEB, 0xBEF9A3F7, 0xC67178F2])
 
   const w = new Uint32Array(64)
   const h = new Uint32Array(8)
-
-  // padding
-  let l = message.byteLength + 8
-  l += (64 - (l % 64))
-
-  const q = new Uint32Array(new ArrayBuffer(l))
+  const q = new Uint32Array(new ArrayBuffer(message.length + 8 + (64 - ((message.length + 8) % 64))))
 
   new Uint8Array(q.buffer).set(message)
   new DataView(q.buffer).setInt8(message.byteLength, 0x80)
@@ -60,37 +53,43 @@ function sha256 (message: Uint8Array): Uint8Array {
   convertEndianness(q)
 
   for (let j = 0; j < q.length; j += 16) {
+    h.set(hh)
     w.set(q.subarray(j))
+
     for (let i = 16; i < 64; i++) {
       const s0 = ((w[i - 15] >>> 7) | (w[i - 15] << 25)) ^ ((w[i - 15] >>> 18) | (w[i - 15] << 14)) ^ (w[i - 15] >>> 3)
       const s1 = ((w[i - 2] >>> 17) | (w[i - 2] << 15)) ^ ((w[i - 2] >>> 19) | (w[i - 2] << 13)) ^ (w[i - 2] >>> 10)
       w[i] = w[i - 16] + s0 + w[i - 7] + s1
     }
 
-    h.set(hh)
-
-    for (let i = 0; i < 64; i++) {
-      const S0 = ((h[0] >>> 2) | (h[0] << 30)) ^ ((h[0] >>> 13) | (h[0] << 19)) ^ ((h[0] >>> 22) | (h[0] << 10))
-      const Ma = (h[0] & h[1]) ^ (h[0] & h[2]) ^ (h[1] & h[2])
-      const S1 = ((h[4] >>> 6) | (h[4] << 26)) ^ ((h[4] >>> 11) | (h[4] << 21)) ^ ((h[4] >>> 25) | (h[4] << 7))
-      const t1 = h[7] + S1 + ((h[4] & h[5]) ^ ((~h[4]) & h[6])) + k[i] + w[i]
-
-      h[7] = h[6]
-      h[6] = h[5]
-      h[5] = h[4]
-      h[4] = h[3] + t1
-      h[3] = h[2]
-      h[2] = h[1]
-      h[1] = h[0]
-      h[0] = t1 + S0 + Ma
-    }
-
-    for (let i = 0; i < 8; i++) {
-      hh[i] += h[i]
-    }
+    shiftRegister(h, k, w)
+    arraySum(hh, h)
   }
 
   return new Uint8Array(convertEndianness(hh))
+}
+
+function shiftRegister (h: Uint32Array, k: Uint32Array, w: Uint32Array) {
+  for (let i = 0; i < 64; i++) {
+    const S0 = ((h[0] >>> 2) | (h[0] << 30)) ^ ((h[0] >>> 13) | (h[0] << 19)) ^ ((h[0] >>> 22) | (h[0] << 10))
+    const Ma = (h[0] & h[1]) ^ (h[0] & h[2]) ^ (h[1] & h[2])
+    const S1 = ((h[4] >>> 6) | (h[4] << 26)) ^ ((h[4] >>> 11) | (h[4] << 21)) ^ ((h[4] >>> 25) | (h[4] << 7))
+    const t1 = h[7] + S1 + ((h[4] & h[5]) ^ ((~h[4]) & h[6])) + k[i] + w[i]
+    h[7] = h[6]
+    h[6] = h[5]
+    h[5] = h[4]
+    h[4] = h[3] + t1
+    h[3] = h[2]
+    h[2] = h[1]
+    h[1] = h[0]
+    h[0] = t1 + S0 + Ma
+  }
+}
+
+function arraySum (a: Uint32Array, b: Uint32Array) {
+  for (let i = 0; i < 8; i++) {
+    a[i] += b[i]
+  }
 }
 
 function convertEndianness (arr: Uint32Array): ArrayBuffer {
